83df436162ae980f94d351f942fa18ece60a07bd
ernestSanchez <ernest.art28@gmail.com>
2020-03-17 14:00:35 +0100

    create landingPage test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {Routes,RouterModule} from '@angular/router';
+import{ HttpClientModule } from '@angular/common/http';
+import {DataService} from  './services/data.service';
+import { GridComponent } from './grid/grid.component';
+
+const RouterConfig : Routes = [
+{"path":"","component": LandingPageComponent},
+{"path":"home","component": LandingPageComponent},
+// {"path":},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingPageComponent,
+    GridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(RouterConfig,{useHash:true}),
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
